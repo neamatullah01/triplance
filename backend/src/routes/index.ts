d175fcae527faecbe707a2455872c782528674ba
neamatullah01 <neamat22205101908@diu.edu.bd>
2026-04-02 12:17:30 +0600

    fix: add env and setup routes

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { AuthRoutes } from '../modules/Auth/auth.route';
 
 type TModuleRoutes = {
   path: string;
@@ -8,7 +9,10 @@ type TModuleRoutes = {
 const router = Router();
 
 const moduleRoutes: TModuleRoutes[] = [
-  
+  {
+    path: '/auth',
+    route: AuthRoutes
+  }
 ];
 
 moduleRoutes.forEach((route) => router.use(route.path, route.route));
